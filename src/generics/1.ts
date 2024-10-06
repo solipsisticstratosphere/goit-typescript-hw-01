479eef15fcdefa5857b44b40abda1ee67b6a95f9
Yaroslav Klimenko <klimyarik13@gmail.com>
2024-10-06 21:52:00 +0300

    123

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-async function fetchData<T extends string | never>(url: T) {
+async function fetchData<T extends string | never>(url: T): Promise<T> {
   try {
     const response = await axios.get(url);
     return response.data;
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,3 @@
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends Object, U extends Object>(objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
